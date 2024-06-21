657c3b6266d9b262a144d001f61871f9af4cf522
UZBEKGIRL <rfarangiz1305@gmail.com>
2024-06-21 20:30:58 +0500

    main uchun

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Route,  Routes } from 'react-router-dom'
+import './App.module.scss'
+import Home from './routes/Home/Home'
+import SignUp from './routes/SignUp/SignUp'
+
+function App() {
+
+  return (
+    <>
+    <Routes>
+    </Routes>
+    </>
+  )
+}
+
+export default App
